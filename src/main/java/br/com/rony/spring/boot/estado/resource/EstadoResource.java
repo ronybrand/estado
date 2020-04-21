4a6ca37b42c7b5ace448ca9da6349d81d4115eab
Rony Brand <ronybrand@gmail.com>
2020-04-21 17:16:18 -0300

    ajustes deploy heroku produção

--- src/main/java/br/com/rony/spring/boot/estado/resource/EstadoResource.java
+++ src/main/java/br/com/rony/spring/boot/estado/resource/EstadoResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import br.com.rony.spring.boot.estado.service.EstadoService;
 @RestController
 @Validated
 @RequestMapping( value = "/estado")
+@CrossOrigin(exposedHeaders="Access-Control-Allow-Origin")
 public class EstadoResource {
 	@Autowired
 	private EstadoService service;
